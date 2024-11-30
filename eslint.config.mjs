ca8a2a33ab2d973744aa4a6ca341d92f765933f1
Welliton Dev <gioriwelliton47@gmail.com>
2024-11-29 22:47:06 -0400

    feat: added code style configs

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    rules: {},
+  },
+  {
+    languageOptions: { globals: globals.node },
+    rules: {
+      // "@typescript-eslint/no-empty-interface": "off",
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
--- src/server/server.ts
+++ src/server/server.ts
@@ -3,7 +3,7 @@ import express from "express";
 const server = express();
 
 server.get("/", (req, res) => {
-  return res.send("Hello Word!");
+    return res.send("Hello Word!");
 });
 
 export { server };
